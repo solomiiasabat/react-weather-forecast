2ee4025a938bfb6babd99ce836410e03f714a219
solomiyasabat <solomiyasabat@gmail.com>
2022-10-16 15:55:24 +0300

    used useEffect, forecast changes properly with every API call

--- src/WeatherDailyForecast.js
+++ src/WeatherDailyForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import EachDayForecast from "./EachDayForecast";
 import "./WeatherDailyForecast.css";
 import axios from "axios";
@@ -6,6 +6,10 @@ import axios from "axios";
 export default function WeatherDailyForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setLoaded(true);
